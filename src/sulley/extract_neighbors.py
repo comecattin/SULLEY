ee395346b0190cdf53d6a451c5c51fd184df34f2
Côme Cattin <come.cattin@ens-lyon.fr>
2024-10-11 14:14:02 +0200

    Find unique non repeating neighbors

--- src/sulley/extract_neighbors.py
+++ src/sulley/extract_neighbors.py
@@ -45,10 +45,54 @@ def get_bonded_neighbors(mol, atom_idx):
         neighbors.append(neighbor.GetIdx())
     return neighbors
 
+def find_unique_non_repeating_neighbors(atom_neighbors, idx_to_sym_class):
+    """Find all the unique non-repeating neighbors of an atom.
+    
+    Parameters
+    ----------
+    atom_neighbors : list
+        List of bonded neighbors.
+    idx_to_sym_class : dict
+        Dictionary of the atom index to the symmetry class.
+    
+    Returns
+    -------
+    sorted_unique_neighbors_no_repeat : list
+        List of unique non-repeating neighbors.
+    """
+    
+    unique_neighbors_type = []
+    unique_neighbors = []
+    for neighbor in atom_neighbors:
+        neighbor_index = neighbor.GetIdx()
+        neighbor_sym_class = idx_to_sym_class[neighbor_index]
+        if neighbor_sym_class not in unique_neighbors_type:
+            unique_neighbors.append(neighbor_index)
+            unique_neighbors_type.append(neighbor_sym_class)
+    sorted_unique_neighbors_no_repeat = sorted(unique_neighbors, reverse=True)
+    return sorted_unique_neighbors_no_repeat
+
 
 
 
 if __name__ == "__main__":
+    
+    import symmetry
+
     smiles = "C1=CC=CC=C1"
     mol = load_molecule(smiles)
-    neighbors = get_bonded_neighbors(mol, 0)
+    idx_to_sym_class, symmetry_class = symmetry.get_canonical_labels(mol)
+    
+    atom_iter = mol.GetAtoms()
+    
+    for atom in atom_iter:
+            
+        atom_index = atom.GetIdx()
+        print('Atom index:',atom_index)
+        atom_neighbors = atom.GetNeighbors()
+        print('Atom neighbors:',[neighbor.GetIdx() for neighbor in atom_neighbors])
+    
+        sorted_unique_neighbors_no_repeat = find_unique_non_repeating_neighbors(atom_neighbors, idx_to_sym_class)
+
+        
+        print('Unique neighbors:',sorted_unique_neighbors_no_repeat)
