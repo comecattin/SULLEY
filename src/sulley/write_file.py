345ff1f1b06bd4f942c81a3f095e3dcdf34056d7
Côme Cattin <come.cattin@ens-lyon.fr>
2024-10-14 14:56:20 +0200

    Writing output module

--- src/sulley/write_file.py
+++ src/sulley/write_file.py
@@ -4,5 +4,55 @@
 Made by C. Cattin, 2024.
 """
 
+def write_peditin_file(
+        mol,
+        idx_to_bisec_then_z_bool, idx_to_trisec_bool,
+        idx_to_bisec_idx, idx_to_trisec_idx,
+        local_frame1, local_frame2,
+        filename:str
+    ):
+
+    f = open(filename, 'w')
+
+    for atom in mol.GetAtoms():
+        atom_idx = atom.GetIdx()
+
+        # Two atoms define the local frame
+        if (
+            not idx_to_bisec_then_z_bool[atom_idx]
+            and not idx_to_trisec_bool[atom_idx]
+        ):
+            f.write(
+                str(atom_idx) + " " +
+                str(local_frame1[atom_idx -1]) + " " +
+                str(local_frame2[atom_idx -1]) + "\n"
+            )
+        
+        # Bisection then z-axis
+        elif (
+            idx_to_bisec_then_z_bool[atom_idx]
+            and not idx_to_trisec_bool[atom_idx]
+        ):
+            bisec_idx = idx_to_bisec_idx[atom_idx]
+            f.write(
+                str(atom_idx) + " " +
+                str(local_frame1[atom_idx -1]) + " -" +
+                str(bisec_idx[0]) + " -" +
+                str(bisec_idx[1]) + "\n"
+            )
+        
+        # Trisection
+        else:
+            trisec_idx = idx_to_trisec_idx[atom_idx]
+            f.write(
+                str(atom_idx) + " -" +
+                str(trisec_idx[0]) + " -" +
+                str(trisec_idx[1]) + " -" +
+                str(trisec_idx[2]) + "\n"
+            )
+
+
+
+
 if __name__ == "__main__":
     pass
