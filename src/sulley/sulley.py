62241934dcb6b4be5a383378332a46f8bc0d16ed
Côme Cattin <come.cattin@ens-lyon.fr>
2024-10-15 16:04:48 +0200

    Client line interface for SULLEY

---
+++ src/sulley/sulley.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""This is the CLI for the Sulley package.
+
+Made by C. Cattin 2024
+"""
+
+import argparse
+from sulley.extract_neighbors import load_molecule
+from sulley.sulley_main import generate_local_frame
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Generate local frames for a molecule.'
+    )
+    parser.add_argument(
+        'smiles',
+        type=str,
+        help='The molecule SMILES to generate the local frame for.'
+    )
+    parser.add_argument(
+        '-o',
+        '--output',
+        type=str,
+        help='The file to write the local frame to. Default is local_frame.txt.',
+        default='local_frame.txt',
+    )
+
+    args = parser.parse_args()
+
+    # Sulley
+    mol = load_molecule(args.smiles)
+    generate_local_frame(mol=mol, filename=args.output)
+
+if __name__ == '__main__':
+    main()
