68e0c5db0365f1a8068dd4679db212f118576b16
Côme Cattin <come.cattin@ens-lyon.fr>
2024-10-17 12:54:34 +0200

    Read tinker xyz file

--- src/sulley/extract_neighbors.py
+++ src/sulley/extract_neighbors.py
@@ -54,6 +54,11 @@ def parse_tinker_xyz(filename: str):
         List of atoms.
     bonds : list
         List of bonds.
+
+    Raises
+    ------
+    ValueError
+        If the number of atoms in the .xyz file does not match the number of atoms in the header.
     """
     atom = []
     bonds = []
@@ -76,13 +81,67 @@ def parse_tinker_xyz(filename: str):
                 if neigh > atom_idx:
                     bonds.append((atom_idx, neigh))
     
+    if len(atom) != num_atoms:
+        raise ValueError(f"Number of atoms in the .xyz file ({len(atom)}) does not match the number of atoms in the header ({num_atoms})")
+
     return atom, bonds
 
 def load_molecule_from_tinker_xyz(filename: str):
+    """Load a molecule from a Tinker .xyz file.
+
+    Parameters
+    ----------
+    filename : str
+        Name of the .xyz file.
+
+    Returns
+    -------
+    mol : rdkit.Chem.Mol
+        RDKit molecule object.
+    """
+
+    atom_type_to_atomic_num = {
+        'H': 1, 'He': 2,
+        'Li': 3, 'Be': 4, 'B': 5, 'C': 6, 'N': 7, 'O': 8, 'F': 9, 'Ne': 10,
+        'Na': 11, 'Mg': 12, 'Al': 13, 'Si': 14, 'P': 15, 'S': 16, 'Cl': 17, 'Ar': 18,
+        'K': 19, 'Ca': 20, 'Sc': 21, 'Ti': 22, 'V': 23, 'Cr': 24, 'Mn': 25, 'Fe': 26,
+        'Co': 27, 'Ni': 28, 'Cu': 29, 'Zn': 30, 'Ga': 31, 'Ge': 32, 'As': 33, 'Se': 34,
+        'Br': 35, 'Kr': 36, 'Rb': 37, 'Sr': 38, 'Y': 39, 'Zr': 40, 'Nb': 41, 'Mo': 42,
+        'Tc': 43, 'Ru': 44, 'Rh': 45, 'Pd': 46, 'Ag': 47, 'Cd': 48, 'In': 49, 'Sn': 50,
+        'Sb': 51, 'Te': 52, 'I': 53, 'Xe': 54, 'Cs': 55, 'Ba': 56, 'La': 57, 'Ce': 58,
+        'Pr': 59, 'Nd': 60, 'Pm': 61, 'Sm': 62, 'Eu': 63, 'Gd': 64, 'Tb': 65, 'Dy': 66,
+        'Ho': 67, 'Er': 68, 'Tm': 69, 'Yb': 70, 'Lu': 71, 'Hf': 72, 'Ta': 73, 'W': 74,
+        'Re': 75, 'Os': 76, 'Ir': 77, 'Pt': 78, 'Au': 79, 'Hg': 80, 'Tl': 81, 'Pb': 82,
+        'Bi': 83, 'Po': 84, 'At': 85, 'Rn': 86, 'Fr': 87, 'Ra': 88, 'Ac': 89, 'Th': 90,
+        'Pa': 91, 'U': 92, 'Np': 93, 'Pu': 94, 'Am': 95, 'Cm': 96, 'Bk': 97, 'Cf': 98,
+        'Es': 99, 'Fm': 100, 'Md': 101, 'No': 102, 'Lr': 103, 'Rf': 104, 'Db': 105,
+        'Sg': 106, 'Bh': 107, 'Hs': 108, 'Mt': 109, 'Ds': 110, 'Rg': 111, 'Cn': 112,
+        'Nh': 113, 'Fl': 114, 'Mc': 115, 'Lv': 116, 'Ts': 117, 'Og': 118
+    }
 
     atom, bonds = parse_tinker_xyz(filename)
     mol = Chem.RWMol()
-    
+
+    # Add atoms to the molecule
+    atom_idx = []
+    for atom_type, coords in atom:
+        atomic_num = atom_type_to_atomic_num[atom_type]
+        idx = mol.AddAtom(Chem.Atom(atomic_num))
+        atom_idx.append(idx)
+
+    # Add bonds to the molecule
+    for idx1, idx2 in bonds:
+        mol.AddBond(idx1, idx2, Chem.rdchem.BondType.SINGLE)
+
+    # Set 3D coordinates
+    conf = Chem.Conformer(len(atom))
+    for i, (_, (x, y, z)) in enumerate(atom):
+        conf.SetAtomPosition(i, (x, y, z))
+    mol.AddConformer(conf)
+
+    return mol.GetMol()
+     
+
 
 def get_bonded_neighbors(mol, atom_idx):
     """Get the bonded neighbors of an atom.
