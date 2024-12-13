9de7d385d9b1d55ee3d90c2d93d0a8876bd89b08
Côme Cattin <come.cattin@ens-lyon.fr>
2024-12-13 17:02:59 +0100

    Test for the ECFP

--- test/test_symmetry.py
+++ test/test_symmetry.py
@@ -19,6 +19,28 @@ def test_symmetry_type_benzene():
         10: [10, 6, 7, 8, 9, 11],
         11: [11, 6, 7, 8, 9, 10]
     }
+    index_to_matching_indices_ref = {k: sorted(v) for k, v in index_to_matching_indices_ref.items()}
+    assert index_to_matching_indices == index_to_matching_indices_ref
+
+def test_symmetry_type_benzene_ecfp():
+    smiles = "C1=CC=CC=C1"
+    mol = extract_neighbors.load_molecule(smiles)
+    index_to_matching_indices = symmetry.compute_symmetry_type_ecfp(mol, radius=3)
+    index_to_matching_indices_ref = {
+        0: [0, 1, 2, 3, 4, 5],
+        1: [1, 0, 2, 3, 4, 5],
+        2: [2, 0, 1, 3, 4, 5],
+        3: [3, 0, 1, 2, 4, 5],
+        4: [4, 0, 1, 2, 3, 5],
+        5: [5, 0, 1, 2, 3, 4],
+        6: [6, 7, 8, 9, 10, 11],
+        7: [7, 6, 8, 9, 10, 11],
+        8: [8, 6, 7, 9, 10, 11],
+        9: [9, 6, 7, 8, 10, 11],
+        10: [10, 6, 7, 8, 9, 11],
+        11: [11, 6, 7, 8, 9, 10]
+    }
+    index_to_matching_indices_ref = {k: sorted(v) for k, v in index_to_matching_indices_ref.items()}
     assert index_to_matching_indices == index_to_matching_indices_ref
 
 def test_get_canonical_labels_benzene():
