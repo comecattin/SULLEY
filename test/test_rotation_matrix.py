1c3df5ccd01b1402fa9b8341bd20fd540eeef3c4
Côme Cattin <come.cattin@ens-lyon.fr>
2024-12-20 13:44:56 +0100

    HCl test for z only local frame

--- test/test_rotation_matrix.py
+++ test/test_rotation_matrix.py
@@ -37,3 +37,24 @@ def test_H2O():
     )
     water_ref[0,0:2,:] *= 1/np.sqrt(2)
     assert np.allclose(water, water_ref)
+
+def test_hcl():
+    """HCl test for z-only
+    
+    No test available for Z-only as the rotation matrix is random
+    Can just check if the last column is the vector between the two atoms
+    """
+    local_frame = [
+            [1, 2, 0],
+            [2, 1, 0],
+        ]
+    coordinates = np.array(
+        [
+            [0.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0],
+        ]
+    )
+    hcl = compute_rotation_matrix(local_frame, coordinates)
+    assert np.allclose(
+        hcl[:,:,2], np.array([[0, 0, 1], [0, 0, -1]])
+    )
