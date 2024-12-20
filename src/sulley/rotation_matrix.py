01acadff9d4eab0b608149af322a22600b1f21ec
Côme Cattin <come.cattin@ens-lyon.fr>
2024-12-20 10:01:52 +0100

    z then x rotation

--- src/sulley/rotation_matrix.py
+++ src/sulley/rotation_matrix.py
@@ -21,7 +21,72 @@ def compute_rotation_matrix(local_frame, coords):
     #--------------------#
     # Z-only local frame #
     #--------------------#
+    z_only_mask = (
+        (local_frame[:, -2] == 0) &
+        (local_frame[:, -1] == 0)
+    )
+    src = np.where(z_only_mask)
+
+
+
+    #----------------------#
+    # Z-then-X local frame #
+    #----------------------#
+    z_then_x_mask = (
+        (local_frame[:, -2] > 0) &
+        (local_frame[:, -1] == 0)
+    )
+    src = np.where(z_then_x_mask)[0]
+    dst = np.abs(local_frame[src, 1:3]) - 1
+    rot_mat[src] = z_then_x_rotation(src, dst, coords)
     
+
+    return rot_mat
+
+def z_then_x_rotation(src, dst, coords):
+    """Compute the rotation matrix for the z-then-x local frame.
+    
+    Parameters
+    -----------
+    src: np.array
+        Source atoms.
+    dst: np.array
+        Destination atoms.
+    coords: np.array
+        Coordinates of the atoms.
+
+    Returns
+    -------
+    rot_mat: np.array
+        Rotation matrix.
+    """
+    
+    z_dst = dst[:, 0]
+    vec_z = coords[z_dst] - coords[src]
+    # Replace the zero vectors by [0, 0, 1]
+    vec_z = np.where(
+        np.linalg.norm(vec_z) == 0,
+        np.array([0, 0, 1]), 
+        vec_z
+    )
+    vec_z = vec_z / np.linalg.norm(vec_z)
+    
+    # ux = atom1 - (atom1 . uz) uz
+    x_dst = dst[:, 1]
+    vec_x = coords[x_dst] - coords[src]
+    # Remplace the zero vectors by [1, 0, 0]
+    vec_x = np.where(
+        np.linalg.norm(vec_x) == 0,
+        np.array([1, 0, 0]),
+        vec_x
+    )
+    vec_x = vec_x - np.sum(vec_x * vec_z) * vec_z
+    vec_x = vec_x / np.linalg.norm(vec_x)
+
+    # uy = uz x ux
+    vec_y = np.cross(vec_z, vec_x)
+
+    rot_mat = np.stack([vec_x, vec_y, vec_z], axis=-1)
 
     return rot_mat
 
